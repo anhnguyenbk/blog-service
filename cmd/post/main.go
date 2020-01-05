c7f936010d0c411b1297eab6bad1159a507d4d54
Anh Nguyen <nghoanganh994@gmail.com>
2020-01-05 16:24:35 +0700

    Allow CORS

--- cmd/post/main.go
+++ cmd/post/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/akrylysov/algnhsa"
 	"github.com/anhnguyenbk/blog-service/internal/post"
 	"github.com/gorilla/mux"
-	// "net/http"
 )
+
+// access control and  CORS middleware
+func accessControlMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	router := mux.NewRouter()
+	router.Use(accessControlMiddleware)
+
 	router.HandleFunc("/", post.PostIndex).Methods("GET")
 	router.HandleFunc("/", post.PostSave).Methods("POST")
 	router.HandleFunc("/slug/{slug}", post.PostShowBySlug).Methods("GET")
--- go.mod
+++ go.mod
@@ -2,14 +2,10 @@ module github.com/anhnguyenbk/blog-service
 
 require (
 	github.com/akrylysov/algnhsa v0.12.1
-	github.com/apex/gateway v1.1.1
-	github.com/aws/aws-lambda-go v1.13.3
+	github.com/aws/aws-lambda-go v1.13.3 // indirect
 	github.com/aws/aws-sdk-go v1.27.0
-	github.com/davyzhang/agw v0.0.0-20180516064718-34fb38c2c135
-	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/mux v1.7.3
-	github.com/json-iterator/go v1.1.9 // indirect
-	github.com/julienschmidt/httprouter v1.3.0
-	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
 )
