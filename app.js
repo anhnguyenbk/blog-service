98e89bb28593a5e293039efeefd4b2637d96d899
anhnguyen <nghoanganh994@gmail.com>
2020-09-08 00:07:26 +0700

    Setup AWS CloudFormation for deployment

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 var bodyParser = require('body-parser')
+var cors = require('body-parser')
 
 const app = express()
 const port = 3000
 // parse application/json
 app.use(bodyParser.json())
+app.use(cors())
 
 const appRoutes = require('./src/routes/index')(app)
 //const db = require('./src/dbs/index')
